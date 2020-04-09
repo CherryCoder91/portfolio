5118d66cbf3398b54f538100adaed77361809ebe
James Pickup <jamespickup2@gmail.com>
2020-04-09 17:12:10 +0100

    Create profile header component

--- projects/portfolio/src/app/angular/routed/home-page/home-page.module.ts
+++ projects/portfolio/src/app/angular/routed/home-page/home-page.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomePageRoutingModule } from './home-page-routing.module';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { ProfileHeaderComponent } from './components/profile-header/profile-header.component';
 
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent, ProfileHeaderComponent],
   imports: [
     CommonModule,
     HomePageRoutingModule
