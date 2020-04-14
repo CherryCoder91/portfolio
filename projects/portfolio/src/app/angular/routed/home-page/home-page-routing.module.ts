9c0aefc422c2568ede043bfb0f20c88a04566180
James Pickup <jamespickup2@gmail.com>
2020-04-14 17:03:33 +0100

    Initial mock out of the first stage structure of the home page
    Assets need improving - performance and quality

--- projects/portfolio/src/app/angular/routed/home-page/home-page-routing.module.ts
+++ projects/portfolio/src/app/angular/routed/home-page/home-page-routing.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { ProfileHeaderComponent } from './components/profile-header/profile-header.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePageComponent
+    component: HomePageComponent,
+    children: [
+      {
+        path: 'about', component: AboutComponent
+      },
+      {
+        path: 'skills', component: SkillsComponent
+      },
+      {
+        path: 'portfolio', component: PortfolioComponent
+      }
+    ]
   }
 ];
-
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- projects/portfolio/src/app/angular/routed/home-page/home-page.module.ts
+++ projects/portfolio/src/app/angular/routed/home-page/home-page.module.ts
@@ -4,13 +4,26 @@ import { CommonModule } from '@angular/common';
 import { HomePageRoutingModule } from './home-page-routing.module';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { ProfileHeaderComponent } from './components/profile-header/profile-header.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatChipsModule } from '@angular/material/chips';
+import { SkillsComponent } from './components/skills/skills.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { HomeNavComponent } from './components/home-nav/home-nav.component';
 
 @NgModule({
-  declarations: [HomePageComponent, ProfileHeaderComponent],
+  declarations: [HomePageComponent, ProfileHeaderComponent, SkillsComponent, AboutComponent, PortfolioComponent, HomeNavComponent],
   imports: [
     CommonModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatChipsModule
   ],
   exports: [HomePageComponent]
 })
